b75fd9fc67ca5bd90ade5c45ed200e52981f021d
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-06-01 22:15:24 +0400

    Add files via upload

---
+++ ModelMigration/ModelMigration/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ModelMigration.DAL;
+using ModelMigration.Models;
+using ModelMigration.ViewModels;
+
+namespace ModelMigration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _db;
+        public HomeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Username = "Kamran";
+            ViewBag.Surname = "Jabiyev";
+            //ViewBag.Students = students;
+            List<Student> students = new List<Student>()
+            { 
+                new Student{Id=1,Name="Mubush",Surname="Memmedzade"},
+                new Student{Id=2,Name="Gunel",Surname="Memmedova"},
+                new Student{Id=3,Name="Abulfat",Surname="Memmedov"}
+            };
+
+            string[] names =
+            {
+                "Rauf",
+                "Nigar",
+                "Fazil"
+            };
+
+            int[] numbers = { 507, 508, 509, 510 };
+
+            HomeVM model = new HomeVM()
+            {
+                Students = students,
+                Names = names,
+                Numbers= numbers
+            };
+            
+
+            return View(model);
+        }
+    }
+}
---
+++ ModelMigration/ModelMigration/Migrations/20200530152255_AddConstrintStuTable.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ModelMigration.Migrations
+{
+    public partial class AddConstrintStuTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Students",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Students",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Students",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
